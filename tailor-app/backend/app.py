b329a5fe06addae2b73616f1fd03f73ec01903a6
nazzaninghazzi <nazzaninghazzi@yahoo.com>
2025-03-19 14:03:51 -0400

    prepopulate db with sample data subject to change

--- tailor-app/backend/app.py
+++ tailor-app/backend/app.py
@@ -1,12 +1,12 @@
 import sentry_sdk
-from flask import Flask, jsonify, send_from_directory
+from flask import Flask, json, jsonify, send_from_directory
 from flask_cors import CORS
 import os
 from dotenv import load_dotenv
 from azure.storage.blob import BlobServiceClient
 import os.path
 from pathlib import Path
-from routes.pin_routes import pin_bp
+from routes.pin_routes import insert_pins, pin_bp
 from routes.chat_routes import chat_bp
 from routes.moodboard_routes import moodboard_bp
 
@@ -73,6 +73,14 @@ if __name__ == "__main__":
     # Ensure database connection is initialized when running as a standalone app
     if mongo_client is None and mongo_db is None:
         initialize_mongo(force_connect=True)
+    
+    json_file_path = "./assets/sample_images.json"  # Adjust path if needed
+    with open(json_file_path, "r") as file:
+        post_data = json.load(file)
+    
+    # prepopulate the database with the sample data under ./assets/sample_images.json
+    with app.test_request_context(method="POST", json=post_data):
+        insert_pins()  
     
     port = int(os.environ.get("PORT", 8000))
     app.run(host='0.0.0.0', port=port)
