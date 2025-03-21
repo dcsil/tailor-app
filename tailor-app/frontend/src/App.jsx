a869eee713aa2c71471fff6a6ddd62313f2b1cfa
Chelsea Wang <chelseawang9@gmail.com>
2025-03-21 12:28:42 -0400

    implemented home page

--- tailor-app/frontend/src/App.jsx
+++ tailor-app/frontend/src/App.jsx
@@ -1,48 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Assets and styling
-import { getBackendUrl } from './utils/env';
-import tailorLogo from './assets/tailor-white-logo.png'
 import './App.css'
 
 // Components
-import Chat from './components/Chat'
-import PromptInput from './components/PromptInput'
-import Footer from './components/Footer'
-
+import Moodboard from './components/Moodboard'
+import HomePage from './pages/HomePage'
 
 
 
 function App() {
-  // temporary for checking backend and frontend integration
-  const [healthStatus, setHealthStatus] = useState(null);
-  const API_URL = getBackendUrl();
-  
-  useEffect(() => {
-    fetch(`${API_URL}/health`)
-      .then(response => response.json()) 
-      .then(data => {
-        setHealthStatus(data.status); 
-      })
-      .catch(error => {
-        console.error("Error fetching health check:", error); 
-      });
-  }, []);
-  
   return (
-    <div className="flex flex-col min-h-screen bg-black text-white">
-      <div className="mb-8">
-        <img src={tailorLogo} className="mx-auto" alt="Tailor logo" />
-      </div>
-      <PromptInput/>
-           
-      {/* <div className="mt-8">
-        <Chat />
-      </div> */}
-
-      <Footer></Footer>
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/moodboard" element={<Moodboard />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ tailor-app/frontend/src/pages/HomePage.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+// Assets and styling
+import tailorLogo from '../assets/tailor-white-logo.png'
+import '../App.css'
+
+// Components
+import PromptInput from '../components/PromptInput'
+import Footer from '../components/Footer'
+
+function HomePage() {
+    return (
+      <div className="flex flex-col min-h-screen bg-black text-white">
+        <div className="mb-8">
+          <img src={tailorLogo} className="mx-auto" alt="Tailor logo" />
+        </div>
+        <PromptInput/>
+        <Footer/>
+      </div>
+  )
+}
+
+export default HomePage
