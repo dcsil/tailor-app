36cd2282a9efe21597cb7bf127841daa0f5af8d5
Nina Huang <nina.hhhuang@gmail.com>
2025-03-25 00:01:35 -0400

    added header

--- tailor-app/frontend/src/App.jsx
+++ tailor-app/frontend/src/App.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable */
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 // Assets and styling
 import { getBackendUrl } from './utils/env';
@@ -9,7 +11,25 @@ import './App.css'
 import Chat from './components/Chat'
 import PromptInput from './components/PromptInput'
 import Footer from './components/Footer'
+import Moodboard from './pages/Moodboard'
+import MyChat from './pages/Chat'
 
+function Home() {
+  return (
+      <div className="flex flex-col min-h-screen bg-black text-white">
+      <div className="mb-8">
+        <img src={tailorLogo} className="mx-auto" alt="Tailor logo" />
+      </div>
+      <PromptInput/>
+           
+      {/* <div className="mt-8">
+        <Chat />
+      </div> */}
+
+      <Footer></Footer>
+    </div>
+  );
+}
 
 
 
@@ -30,19 +50,26 @@ function App() {
   }, []);
   
   return (
-    <div className="flex flex-col min-h-screen bg-black text-white">
-      <div className="mb-8">
-        <img src={tailorLogo} className="mx-auto" alt="Tailor logo" />
-      </div>
-      <PromptInput/>
+    // <div className="flex flex-col min-h-screen bg-black text-white">
+    //   <div className="mb-8">
+    //     <img src={tailorLogo} className="mx-auto" alt="Tailor logo" />
+    //   </div>
+    //   <PromptInput/>
            
-      {/* <div className="mt-8">
-        <Chat />
-      </div> */}
+    //   {/* <div className="mt-8">
+    //     <Chat />
+    //   </div> */}
 
-      <Footer></Footer>
+    //   <Footer></Footer>
       
-    </div>
+    // </div>
+    <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/moodboard" element={<Moodboard />} />
+                <Route path="/mychat" element={<MyChat />} />
+            </Routes>
+    </Router>
   )
 }
 
